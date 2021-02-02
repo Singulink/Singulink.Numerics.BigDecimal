5dc919794820a0d2ca56dafeeb78c296aa25ebb2
Mike Marynowski <mikem@singulink.com>
2021-02-02 00:08:42 -0500

    Added test for exact double conversion.

---
+++ Source/Singulink.Numerics.BigDecimal.Tests/ConvertToBigDecimalTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Singulink.Numerics.Tests
+{
+    [TestClass]
+    public class ConvertToBigDecimalTests
+    {
+        [TestMethod]
+        public void FromDoubleExact()
+        {
+            Assert.AreEqual("0.1000000000000000055511151231257827021181583404541015625", BigDecimal.FromDouble(0.1, true).ToString());
+        }
+    }
+}
